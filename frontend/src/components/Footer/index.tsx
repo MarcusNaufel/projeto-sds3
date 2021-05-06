478bb56a802de109bbeda3a752d93e31dfbcdd23
Marcus Naufel <mvnaufel@gmail.com>
2021-05-06 19:56:46 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/MarcusNaufel" target="_blank" rel="noreferrer">Marcus Naufel</a> <small> <a href="https://www.instagram.com/marcus_naufel/" target="_blank" rel="noreferrer">@marcus_naufel.ig</a></small></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
